5b836d1208e9bb6ce815a3ca3eb1cea80e2dfe99
talhahzahid <talhazahid415@gmail.com>
2024-09-15 01:09:36 +0500

    Ecommerce

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,23 +1,34 @@
 
 
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 const Home = () => {
+  const [data , setData] = useState(null)
     useEffect(()=>{
         axios('https://fakestoreapi.com/products')
          .then((res)=>{
             console.log(res.data);
+            setData(res.data)
          }).catch((err)=>{
             console.log(err);
             
          })
     },[])
   return (
-    <>
-    <h1>Home</h1>
-    </>
+ <>
+ <h1>Home </h1>
+ <div>
+  {
+    data ? (
+    data.map((item)=>(
+      <div key={item.id}><h1>{item.price}</h1></div>
+    ))
+    ):(<h1>loading....</h1>)
+  }
+ </div>
+ </>
   )
 }
 
